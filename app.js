58938c71ffd92bd57396cfab355ab59f34a4328f
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-10-17 16:44:32 +0200

    updated to use SQS

--- app.js
+++ app.js
@@ -1,9 +1,74 @@
 import express from "express";
+import { SQSClient, ReceiveMessageCommand, DeleteMessageCommand } from "@aws-sdk/client-sqs";
+
+const PORT = 3000;
+const QUEUE_URL = "https://sqs.eu-west-3.amazonaws.com/340752836075/DietiEstates-Insertions-Queue";
+
+const clients = new Set();
+const sqsClient = new SQSClient({ region: 'eu-west-3' });
 
 const app = express();
-const PORT = 3000;
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.setHeader('X-Accel-Buffering', 'no');
+
+    res.write('data: {"type":"connected","message":"Connected to notification stream"}\n\n');
+
+    clients.add(res);
+    console.log('Client connected. Total clients:', clients.size);
+
+    req.on('close', () => {
+        clients.delete(res);
+        console.log('Client disconnected. Total clients:', clients.size);
+    });
 });
 
-app.listen(PORT);
+function broadcastNotification(notification) {
+    const data = `data: ${JSON.stringify(notification)}\n\n`;
+
+    clients.forEach(client => {
+        try {
+            client.write(data);
+        } catch (error) {
+            console.error('Error writing to client:', error);
+            clients.delete(client);
+        }
+    });
+}
+
+async function pollSQS() {
+    if (clients.size === 0) {
+        return; // No clients connected, skip polling
+    }
+
+    const response = await sqsClient.send(new ReceiveMessageCommand({
+        QueueUrl: QUEUE_URL,
+        MaxNumberOfMessages: 10,
+        WaitTimeSeconds: 20, // Long polling
+        MessageAttributeNames: ['All'],
+        AttributeNames: ['All']
+    }));
+
+    if (!response.Messages) {
+        return;
+    }
+
+    response.Messages.forEach(async message => {
+        console.log('Received message from SQS:', message);
+
+        broadcastNotification(message);
+
+        await sqsClient.send(new DeleteMessageCommand({
+            QueueUrl: QUEUE_URL,
+            ReceiptHandle: message.ReceiptHandle
+        }));
+    });
+}
+
+setInterval(pollSQS, 1000);
+
+app.listen(PORT, () => {
+    console.log(`SSE service is running on http://localhost:${PORT}`);
+});
